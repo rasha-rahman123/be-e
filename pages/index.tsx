f8302507ab5a89fba5a473d580295f2d917749a7
Rasha Rahman <rasharahman@rashas-mbp.lan>
2021-01-12 16:16:59 -0800

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,40 @@
 import Link from 'next/link'
+import { useState } from 'react';
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
+async function fetchName() {
+  const res = await fetch('/api/users/getName');
+  const brin = await res.json();
+  const rerBrin : String[] = await brin['combined']
+  
+  return rerBrin
+}
+
+const IndexPage = () => {
+  const [name,setName] = useState<String>()
+  const [name1,setName1] = useState<String>()
+
+  const fetchFunction = async () => {
+    const d : String[] = await fetchName()
+    //@ts-ignore
+    setName(await d.bLeR)
+    //@ts-ignore
+    setName1(await d.eRbL)
+  }
+
+  return (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 style={{textAlign: 'center'}}>Bengali-English Name Combiner</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+   {!name &&   <Link href="#">
+        <a onClick={() => fetchFunction()}>Get Name</a>
+      </Link>}
     </p>
+    <h3 style={{textAlign: 'center'}}>
+      {name && name} <br />
+      {name1 && name1}
+    </h3>
   </Layout>
-)
+)}
 
 export default IndexPage
